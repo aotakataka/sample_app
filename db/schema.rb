a1892c6d4ce5d4449057c05602902f62d32b1f1e
aotaka <130732987+aotakataka@users.noreply.github.com>
2025-05-20 17:43:54 +0900

    Validate Unique

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_16_063858) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_20_081230) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
