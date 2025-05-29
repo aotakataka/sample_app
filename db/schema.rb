0b9d674a2c0cdc16494dc8dabe751348aef2888f
aotaka <130732987+aotakataka@users.noreply.github.com>
2025-05-30 02:34:23 +0900

    Add Microposts_model and Show microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_27_055030) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_29_134942) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -26,4 +35,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_27_055030) do
     t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "microposts", "users"
 end
