555db74b8b2e3900e92e87d75d0c75c6e77e9409
aotaka <130732987+aotakataka@users.noreply.github.com>
2025-05-31 02:27:50 +0900

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@ puts "Deleting all users..."
 User.delete_all
 
 # メインのサンプルユーザーを1人作成する
+puts "Create one user..."
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
@@ -15,6 +16,7 @@ User.create!(name:  "Example User",
              activated_at: Time.zone.now)
 
 # 追加のユーザーをまとめて生成する
+puts "Create additional users..."
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
@@ -28,8 +30,18 @@ User.create!(name:  "Example User",
 end
 
 # ユーザーの一部を対象にマイクロポストを生成する
+puts "Create some microposts..."
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# ユーザーフォローのリレーションシップを作成する
+puts "Create some follow relationships..."
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
